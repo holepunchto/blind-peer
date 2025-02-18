49cb2ac12a1582817cbfa4a544f8bfc9c181575e
Mathias Buus <mathiasbuus@gmail.com>
2025-02-18 17:07:34 +0100

    Refactor, does mirroring and self-described mailboxes now (#26)
    * add new impl
    
    * fix bugs
    
    * fix standard
    
    * Clean up + add basic test
    
    * Extend test to verify the blind peer downloaded the core
    
    ---------
    
    Co-authored-by: HDegroote <75906619+HDegroote@users.noreply.github.com>

---
+++ build.js
@@ -0,0 +1,299 @@
+const path = require('path')
+
+const Hyperschema = require('hyperschema')
+const HyperDB = require('hyperdb/builder')
+
+const SCHEMA_DIR = path.join(__dirname, './spec/hyperschema')
+const DB_DIR = path.join(__dirname, './spec/hyperdb')
+
+const schema = Hyperschema.from(SCHEMA_DIR)
+
+const peerSchema = schema.namespace('blind-peer')
+
+peerSchema.register({
+  name: 'auth',
+  fields: [
+    {
+      name: 'swarming',
+      type: 'fixed32',
+      required: true
+    },
+    {
+      name: 'encryption',
+      type: 'fixed32',
+      required: true
+    }
+  ]
+})
+
+peerSchema.register({
+  name: 'digest',
+  fields: [
+    {
+      name: 'referrers',
+      type: 'uint',
+      required: true
+    },
+    {
+      name: 'cores',
+      type: 'uint',
+      required: true
+    },
+    {
+      name: 'bytesAllocated',
+      type: 'uint',
+      required: true
+    },
+    {
+      name: 'flushed',
+      type: 'bool'
+    }
+  ]
+})
+
+peerSchema.register({
+  name: 'mailbox',
+  fields: [
+    {
+      name: 'version',
+      type: 'uint',
+      required: true
+    },
+    {
+      name: 'seed',
+      type: 'fixed32',
+      required: true
+    },
+    {
+      name: 'referrer',
+      type: 'fixed32',
+      required: false
+    },
+    {
+      name: 'blockEncryptionKey',
+      type: 'fixed32',
+      required: false
+    }
+  ]
+})
+
+peerSchema.register({
+  name: 'add-core-request',
+  flagsPosition: 0,
+  fields: [
+    {
+      name: 'key',
+      type: 'fixed32',
+      required: true
+    },
+    {
+      name: 'referrer',
+      type: 'fixed32',
+      required: false
+    },
+    {
+      name: 'deprecatedAutobase',
+      type: 'fixed32',
+      required: false
+    },
+    {
+      name: 'deprecatedAutobaseBlockKey',
+      type: 'fixed32',
+      required: false
+    },
+    {
+      name: 'priority',
+      type: 'uint',
+      required: false
+    },
+    {
+      name: 'announce',
+      type: 'bool',
+      required: false
+    }
+  ]
+})
+
+peerSchema.register({
+  name: 'post-to-mailbox-request',
+  fields: [
+    {
+      name: 'mailbox',
+      type: 'buffer',
+      required: true
+    },
+    {
+      name: 'message',
+      type: 'buffer',
+      required: true
+    }
+  ]
+})
+
+peerSchema.register({
+  name: 'wakeup-entry',
+  compact: true,
+  fields: [
+    {
+      name: 'key',
+      type: 'fixed32',
+      required: true
+    },
+    {
+      name: 'length',
+      type: 'uint',
+      required: true
+    }
+  ]
+})
+
+peerSchema.register({
+  name: 'wakeup-reply',
+  fields: [
+    {
+      name: 'version',
+      type: 'uint',
+      required: true
+    },
+    {
+      name: 'type',
+      type: 'uint',
+      required: true
+    },
+    {
+      name: 'writers',
+      type: '@blind-peer/wakeup-entry',
+      array: true,
+      required: true
+    }
+  ]
+})
+
+peerSchema.register({
+  name: 'core',
+  fields: [
+    {
+      name: 'key',
+      type: 'fixed32',
+      required: true
+    },
+    {
+      name: 'length',
+      type: 'uint',
+      required: true
+    },
+    {
+      name: 'bytesAllocated',
+      type: 'uint',
+      required: true
+    },
+    {
+      name: 'updated',
+      type: 'uint',
+      required: true
+    },
+    {
+      name: 'active',
+      type: 'uint',
+      required: true
+    },
+    {
+      name: 'priority',
+      type: 'uint',
+      required: true
+    },
+    {
+      name: 'announce',
+      type: 'bool',
+      required: false
+    },
+    {
+      name: 'referrer',
+      type: 'fixed32',
+      required: false
+    }
+  ]
+})
+
+Hyperschema.toDisk(schema)
+
+const db = HyperDB.from(SCHEMA_DIR, DB_DIR)
+const peerDb = db.namespace('blind-peer')
+
+peerDb.require(path.join(__dirname, './lib/db-actions.js'))
+
+peerDb.collections.register({
+  name: 'auth',
+  schema: '@blind-peer/auth',
+  key: []
+})
+
+peerDb.collections.register({
+  name: 'digest',
+  schema: '@blind-peer/digest',
+  key: []
+})
+
+peerDb.collections.register({
+  name: 'cores',
+  schema: '@blind-peer/core',
+  key: ['key']
+})
+
+peerDb.indexes.register({
+  name: 'cores-by-referrer',
+  collection: '@blind-peer/cores',
+  key: {
+    type: {
+      fields: [
+        {
+          name: 'referrer',
+          type: 'fixed32'
+        },
+        {
+          name: 'updated',
+          type: 'uint'
+        },
+        {
+          name: 'core',
+          type: 'fixed32'
+        }
+      ]
+    },
+    map: 'mapByReferrer'
+  },
+  unique: true
+})
+
+peerDb.indexes.register({
+  name: 'cores-by-announce',
+  collection: '@blind-peer/cores',
+  key: {
+    type: {
+      fields: [
+        {
+          name: 'core',
+          type: 'fixed32'
+        }
+      ]
+    },
+    map: 'mapByAnnounce'
+  },
+  unique: true
+})
+
+peerDb.indexes.register({
+  name: 'cores-by-activity',
+  collection: '@blind-peer/cores',
+  key: ['priority', 'active'],
+  unique: false
+})
+
+peerDb.indexes.register({
+  name: 'cores-by-bytes-allocated',
+  collection: '@blind-peer/cores',
+  key: ['bytesAllocated'],
+  unique: false
+})
+
+HyperDB.toDisk(db)
--- test/test.js
+++ test/test.js
@@ -1,126 +1,52 @@
 const test = require('brittle')
 const setupTestnet = require('hyperdht/testnet')
-const Hyperswarm = require('hyperswarm')
-const Autobase = require('autobase')
 const Corestore = require('corestore')
-const b4a = require('b4a')
-const hypCrypto = require('hypercore-crypto')
 const tmpDir = require('test-tmp')
+const { once } = require('events')
+const b4a = require('b4a')
+const Client = require('@holepunchto/blind-peering/lib/client')
+const Hyperswarm = require('hyperswarm')
 const BlindPeer = require('..')
-const Client = require('@holepunchto/blind-peer-client/lib/client')
 
 const DEBUG = false
 let clientCounter = 0 // For clean teardown order
 
-test('client can use a blind-peer to add an autobase message', async t => {
-  t.plan(2)
-
+test('client can use a blind-peer to add a core', async t => {
   const { bootstrap } = await getTestnet(t)
 
   const { blindPeer } = await setupBlindPeer(t, bootstrap)
+  await blindPeer.listen()
   await blindPeer.swarm.flush()
 
-  const { base, swarm: baseSwarm, mailboxId } = await setupAutobase(t, bootstrap, blindPeer.publicKey)
+  let coreKey = null
+  const coreAddedProm = once(blindPeer, 'add-core')
+  coreAddedProm.catch(() => {})
+  let client = null
+  {
+    const { core, swarm } = await setupCoreHolder(t, bootstrap)
+    swarm.joinPeer(blindPeer.publicKey) // TODO: clean flow for setting up corestore replication (blind-peer client works at dht level, so no swarm connection event)
+    client = new Client(blindPeer.publicKey, { dht: swarm.dht })
+    await client.ready()
 
-  baseSwarm.joinPeer(blindPeer.publicKey)
-  await new Promise(resolve => { // ensure mailbox fully registered
-    blindPeer.on('add-mailbox-response', (req, res) => {
-      // Set when the mailbox is fully open
-      // (which with the autobase connection flow currently happens
-      //  after 2 add-mailbox requests when starting from scratch)
-      if (res.blockEncryptionKey) resolve()
-    })
-  })
+    coreKey = core.key
+    await client.addCore(coreKey)
+  }
 
-  base.view.once('append', async () => {
-    const message = await base.view.get(base.view.length - 1)
-    t.alike(
-      message,
-      { hi: 'there' },
-      'Message processed by autobase'
-    )
-  })
+  const [record] = await coreAddedProm
+  t.alike(record.key, coreKey, 'added the core')
+  t.is(record.priority, 0, '0 Default priority')
+  t.is(record.announce, false, 'default no announce')
+  await client.close()
 
-  const swarm = new Hyperswarm({ bootstrap })
-  swarm.on('connection', async (conn) => {
-    try {
-      const client = new Client(conn)
-      await client.postToMailbox({
-        id: mailboxId,
-        message: b4a.from(JSON.stringify({ hi: 'there' }))
-      })
-      swarm.leavePeer(blindPeer.publicKey)
-      await client.close()
-      await swarm.destroy()
-    } catch (e) {
-      console.error('unexpected error while posting to mailbox')
-      console.error(e)
-      t.fail('error while posting to mailbox')
-      return
-    }
-
-    t.pass('Successfully posted to mailbox')
-  })
-  swarm.joinPeer(blindPeer.publicKey)
-})
-
-test('can send autobase message with restarted blind-peer', async t => {
-  t.plan(2)
-
-  const { bootstrap } = await getTestnet(t)
-
-  let blindPeerStorage = null
-  const { blindPeer: blindPeerRun1, storage } = await setupBlindPeer(t, bootstrap)
-  blindPeerStorage = storage
-  await blindPeerRun1.swarm.flush()
-
-  const { base, swarm: baseSwarm, mailboxId } = await setupAutobase(t, bootstrap, blindPeerRun1.publicKey)
-
-  baseSwarm.joinPeer(blindPeerRun1.publicKey)
-  await new Promise(resolve => { // ensure mailbox fully registered
-    blindPeerRun1.on('add-mailbox-response', (req, res) => {
-      // Set when the mailbox is fully open
-      // (which with the autobase connection flow currently happens
-      //  after 2 add-mailbox requests when starting from scratch)
-      if (res.blockEncryptionKey) resolve()
-    })
-  })
-
-  await blindPeerRun1.close()
-
-  const { blindPeer } = await setupBlindPeer(t, bootstrap, { storage: blindPeerStorage })
-  await blindPeer.swarm.flush()
-
-  base.view.once('append', async () => {
-    const message = await base.view.get(base.view.length - 1)
-    t.alike(
-      message,
-      { hi: 'there' },
-      'Message processed by autobase'
-    )
-  })
-
-  const swarm = new Hyperswarm({ bootstrap })
-  swarm.on('connection', async (conn) => {
-    try {
-      const client = new Client(conn)
-      await client.postToMailbox({
-        id: mailboxId,
-        message: b4a.from(JSON.stringify({ hi: 'there' }))
-      })
-      swarm.leavePeer(blindPeer.publicKey)
-      await client.close()
-      await swarm.destroy()
-    } catch (e) {
-      console.error('unexpected error while posting to mailbox')
-      console.error(e)
-      t.fail('error while posting to mailbox')
-      return
-    }
-
-    t.pass('Successfully posted to mailbox')
-  })
-  swarm.joinPeer(blindPeer.publicKey)
+  {
+    const { swarm, store } = await setupPeer(t, bootstrap)
+    const core = store.get({ key: coreKey })
+    await core.ready()
+    swarm.joinPeer(blindPeer.publicKey, { dht: swarm.dht })
+    await swarm.flush()
+    const block = await core.get(1)
+    t.is(b4a.toString(block), 'Block 1', 'Can download the core from the blind peer')
+  }
 })
 
 async function getTestnet (t) {
@@ -132,9 +58,43 @@ async function getTestnet (t) {
   return testnet
 }
 
+async function setupPeer (t, bootstrap) {
+  const storage = await tmpDir(t)
+  const swarm = new Hyperswarm({ bootstrap })
+  const store = new Corestore(storage)
+
+  const order = clientCounter++
+  swarm.on('connection', c => {
+    if (DEBUG) console.log('(CORE HOLDER) connection opened')
+    store.replicate(c)
+    c.on('error', (e) => {
+      if (DEBUG) console.warn(`Swarm error: ${e.stack}`)
+    })
+  })
+  t.teardown(async () => {
+    await swarm.destroy()
+    await store.close()
+  }, { order })
+
+  return { swarm, store }
+}
+
+async function setupCoreHolder (t, bootstrap) {
+  const { swarm, store } = await setupPeer(t, bootstrap)
+
+  const core = store.get({ name: 'core' })
+  await core.append('Block 0')
+  await core.append('Block 1')
+  swarm.join(core.discoveryKey)
+
+  return { swarm, store, core }
+}
+
 async function setupBlindPeer (t, bootstrap, { storage } = {}) {
   if (!storage) storage = await tmpDir(t)
-  const peer = new BlindPeer(storage)
+
+  const swarm = new Hyperswarm({ bootstrap })
+  const peer = new BlindPeer(storage, { swarm })
 
   if (DEBUG) {
     peer.on('add-mailbox-request', (req) => {
@@ -154,9 +114,10 @@ async function setupBlindPeer (t, bootstrap, { storage } = {}) {
   const order = clientCounter++
   t.teardown(async () => {
     await peer.close()
+    await swarm.destroy()
   }, { order })
 
-  await peer.listen({ bootstrap })
+  await peer.listen()
   if (DEBUG) {
     peer.swarm.on('connection', () => {
       console.log('Blind peer connection opened')
@@ -164,70 +125,4 @@ async function setupBlindPeer (t, bootstrap, { storage } = {}) {
   }
 
   return { blindPeer: peer, storage }
-}
-
-async function setupAutobase (t, bootstrap, blindPeerKey) {
-  const storage = await tmpDir(t)
-  const base = new Autobase(
-    new Corestore(storage),
-    null,
-    {
-      encryptionKey: Buffer.alloc(30).fill('secret'),
-      open (store) {
-        return store.get('view', { valueEncoding: 'json' })
-      },
-      async apply (nodes, view, base) {
-        for (const node of nodes) {
-          const jsonValue = JSON.parse(node.value.toString())
-          if (jsonValue.add) {
-            await base.addWriter(Buffer.from(jsonValue.key, 'hex'), { indexer: false })
-          } else {
-            view.append(jsonValue)
-          }
-        }
-      }
-    }
-  )
-
-  await base.ready()
-
-  const id = hypCrypto.randomBytes(32)
-
-  const swarm = new Hyperswarm({ bootstrap })
-  swarm.on('connection', async c => {
-    if (DEBUG) console.log('autobase swarm connection opened')
-    base.store.replicate(c)
-    if (!c.remotePublicKey.equals(blindPeerKey)) return
-
-    const peer = new Client(c)
-
-    const info = await peer.addMailbox({ id, autobase: base.key })
-
-    if (info.open === false) {
-      const message = b4a.from(
-        JSON.stringify({ add: true, key: info.writer.toString('hex') })
-      )
-      await base.append(message)
-      await base.update()
-
-      const core = base.store.get({ key: info.writer, active: false })
-      await core.setEncryptionKey(base.encryptionKey)
-      const req = { id, autobase: base.key, blockEncryptionKey: core.encryption.blockKey }
-      await core.close()
-
-      await peer.addMailbox(req)
-
-      if (DEBUG) console.log('autobase opened mailbox')
-    }
-
-    await peer.close()
-  })
-
-  const order = clientCounter++
-  t.teardown(async () => {
-    await swarm.destroy()
-    await base.close()
-  }, { order })
-
-  return { base, swarm, mailboxId: id }
 }
