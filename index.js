3c13388d7736f0c8c49698a8a95b9ed448951544
hdegroote <75906619+HDegroote@users.noreply.github.com>
2026-08-16 22:15:48 +0200

    Add runtime to notification-sent event

--- index.js
+++ index.js
@@ -991,6 +991,7 @@ class BlindPeer extends ReadyResource {
 
   async _onnotification(stream, request) {
     this.stats.notificationsRx++
+    const startTime = Date.now()
     this.emit('notification-rx', request, stream)
 
     if (!this.gatewayPool) {
@@ -1037,7 +1038,7 @@ class BlindPeer extends ReadyResource {
       )
 
       this.stats.notificationsSent++
-      this.emit('notification-sent', request, payload, stream)
+      this.emit('notification-sent', request, payload, stream, Date.now() - startTime)
     } finally {
       await core.close()
     }
