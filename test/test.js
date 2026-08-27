2c7766b6cdffd95714946f056aeafc6d91c4f2fc
Thang Nguyen <thangnv77@gmail.com>
2026-08-27 15:11:38 +0700

    update test name

--- test/test.js
+++ test/test.js
@@ -187,7 +187,7 @@ test('blind-peer can set treeCache options for corestore', async (t) => {
   t.is(blindPeer.notificationErrorSnapshotDelay, 30_000, 'got snapshot delay default')
 })
 
-test('client can ask a blind-peer to create and forward a push notification', async (t) => {
+test('push notification timeout when getting block does not close the connection and emits a delayed error snapshot', async (t) => {
   const { bootstrap } = await getTestnet(t)
 
   const { gateway, sentMessages } = await setupPushGateway(t, bootstrap)
