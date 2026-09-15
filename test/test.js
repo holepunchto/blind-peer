bfafd9ed9741c528d31971743dbe53bb95f3e6d7
Ernestas <volstas@pm.me>
2026-09-15 08:57:21 +0300

    Test removing new listeners

--- test/test.js
+++ test/test.js
@@ -3468,12 +3468,9 @@ test('destroying a peer in blind-peering clears core listeners', async (t) => {
   t.is(peer.cores.size, 0, 'destroy() clears the cores map of the peer')
 })
 
-test('destroying peer in blind-peering clears autobase listeners', async (t) => {
+test.solo('destroying peer in blind-peering clears autobase listeners', async (t) => {
   const { bootstrap } = await getTestnet(t)
-
-  const { blindPeer } = await setupBlindPeer(t, bootstrap)
-  await blindPeer.swarm.flush()
-
+  const { blindPeer } = await initBlindPeer(t, bootstrap)
   const { swarm, store, base } = await setupAutobaseHolder(t, bootstrap)
   t.teardown(() => base.close())
   await base.append({ hello: 'world' })
@@ -3481,13 +3478,17 @@ test('destroying peer in blind-peering clears autobase listeners', async (t) => 
   const client = createClient(t, swarm.dht, store, { keys: [blindPeer.publicKey] })
 
   t.is(base.listenerCount('close'), 0, 'base 0 "close" listeners initially')
-  t.is(base.listenerCount('writer'), 0, 'base 0 "writer" liteners initially')
-  t.is(base.core.listenerCount('migrate'), 0, 'base core 0 "migrate" liteners initially')
+  t.is(base.listenerCount('writer'), 0, 'base 0 "writer" listeners initially')
+  t.is(base.listenerCount('anchor'), 0, 'base 0 "anchor" listeners initially')
+  t.is(base.listenerCount('appending'), 0, 'base 0 "appending" listeners initially')
+  t.is(base.core.listenerCount('migrate'), 0, 'base core 0 "migrate" listeners initially')
 
   await client.addAutobase(base)
 
   t.is(base.listenerCount('close'), 1, 'base 1 "close" listener after adding')
-  t.is(base.listenerCount('writer'), 1, 'base 1 "writer" liteners after adding')
+  t.is(base.listenerCount('writer'), 1, 'base 1 "writer" listeners after adding')
+  t.is(base.listenerCount('anchor'), 1, 'base 1 "anchor" listeners after adding')
+  t.is(base.listenerCount('appending'), 1, 'base 1 "appending" listeners after adding')
   t.is(base.core.listenerCount('migrate'), 1, 'base core 1 "migrate" listener after adding')
 
   const peer = client.blindPeers.get(b4a.toString(blindPeer.publicKey, 'hex'))
@@ -3497,6 +3498,8 @@ test('destroying peer in blind-peering clears autobase listeners', async (t) => 
   t.is(peer.destroyed, true, 'closing blind-peering destroyed the peer')
   t.is(base.listenerCount('close'), 0, 'base 0 "close" listeners after peer is destroyed')
   t.is(base.listenerCount('writer'), 0, 'base 0 "writer" listeners after peer is destroyed')
+  t.is(base.listenerCount('anchor'), 0, 'base 0 "anchor" listeners after peer is destroyed')
+  t.is(base.listenerCount('appending'), 0, 'base 0 "appending" listeners after peer is destroyed')
   t.is(
     base.core.listenerCount('migrate'),
     0,
